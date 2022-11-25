a6f274a71480d7b25d60fa0dfc9362387123db8a
Akshat Singhal <singhal.akshat.2017453@gmail.com>
2022-11-26 01:15:22 +0530

    ALl files updated

--- again_zappa/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-ime_stamp = time.time()
-    # print(time_stamp)
-    # tm= float(r['message']['timestamp'])
-    # tm = tm/1000
-    # print(tm)
-    # delta = time_stamp - tm
-    # if(delta > 120) :
-    #     return ""
